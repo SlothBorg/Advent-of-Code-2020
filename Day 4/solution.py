0c07ded20e565f513a92de99f5c2098915b23e87
EJ Fernandez <elias.fernandez@cisecurity.org>
2020-12-04 12:11:06 -0500

    Day 4 Part 2
    with robust regex!

--- Day 4/solution.py
+++ Day 4/solution.py
@@ -1,3 +1,5 @@
+import re
+
 def get_data():
     with open('data/input') as f:
         return f.read().split('\n\n')
@@ -5,20 +7,45 @@ def get_data():
 
 def parse_passport(dirty_passport):
     passport = dirty_passport.split()
-    return set([fields.split(':')[0] for fields in passport])
+    return [fields.split(':') for fields in passport]
 
 
-def validate_passport(passport, requirements):
-    if len(passport) == 8:
-        return True
-    else:
-        return all(elem in passport  for elem in requirements)
+def validate_passport_fields(passport_fields, requirements):
+    field_count = 0
+    for field in passport_fields:
+        if field[0] in requirements:
+            field_count +=1
+    return field_count >= 7
+
+
+def validate_passport_data(passport_data, requirements):
+    for data in passport_data:
+        if data[0] == 'cid':
+            pass
+        elif re.search(requirements[data[0]], data[1]) == None:
+            return False
+    return True
 
 
 passports = get_data()
-requirements = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-valid_passports = 0
-for passport in passports:
-    valid_passports += validate_passport(parse_passport(passport), requirements)
+field_requirements = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+passports_with_valid_fields = 0
+passports_with_valid_data = 0
+data_requirements = {
+    'byr' : '((^19[2-9]\d$)|((^200[0-2])$))',
+    'iyr' : '(^(201\d$)|(^2020)$)',
+    'eyr' : '(^(202\d$)|(^2030)$)',
+    'hgt' : '(((1(([5-8]\d)|(9[0-3])))cm)|(((59)|(6\d)|(7[0-6]))in))',
+    'hcl' : '#(\d|[a-f]){6}',
+    'ecl' : '(amb|blu|brn|gry|grn|hzl|oth)',
+    'pid' : '^\d{9}$'
+}
 
-print(valid_passports)
+for passport in passports:
+    passport_data = parse_passport(passport)
+    if validate_passport_fields(passport_data, field_requirements):
+        passports_with_valid_fields += 1
+        passports_with_valid_data += validate_passport_data(passport_data, data_requirements)
+
+print(passports_with_valid_fields)
+print(passports_with_valid_data)
